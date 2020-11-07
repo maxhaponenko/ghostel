24116322d27e48e9fa73da9ec78fbb45451d379e
Max Haponenko <gaponenko.mm@gmail.com>
2020-11-07 23:59:43 +0200

    Layout updates

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ export default function Home() {
   return (
     <div className="min-h-screen lg:flex text-lg">
       {/* left side */}
-      <div className="lg:w-1/2 relative z-10 flex flex-col justify-center px-10 lg:px-20 py-20 lg:py-0 text-left">
-        <h2 className="inter text-4xl mb-3 font-bold text-gray-800">
-          Welcome home, dear traveler!{" "}
-          {/* <span className="block text-blue-500 text-2xl font-normal">
-            Deployed to DigitalOcean
-          </span> */}
+      <div className="lg:w-1/3 relative z-10 flex flex-col justify-center px-10 lg:px-20 py-20 lg:py-0 text-left left-content">
+
+        <div className="mb-5">
+          <img src="/logo.png" alt="Logo image"></img>
+        </div>
+
+        <h2 className="inter text-4xl mb-3 font-bold text-gray-800" style={{lineHeight: 1.2}}>
+          Welcome home, dear traveler!
         </h2>
 
         <p className="text-gray-700 mb-6">
@@ -16,7 +18,7 @@ export default function Home() {
       </div>
 
       {/* right side */}
-      <div className="lg:w-1/2 relative">
+      <div className="lg:w-2/3 relative">
         <svg
           className="hidden lg:block text-white fill-current absolute h-full transform -translate-x-1/2 w-48 z-10"
           viewBox="0 0 100 100"
@@ -26,7 +28,7 @@ export default function Home() {
         </svg>
 
         <img
-          src="/background2.jpg"
+          src="/background3.jpg"
           alt="Ocean Image"
           className="lg:absolute object-cover lg:inset-y-0 lg:right-0 lg:h-full lg:w-full"
         />
