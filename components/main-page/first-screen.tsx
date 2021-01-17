015d95226118aee49e3ae649cd0dc2a26aa1960c
Max Haponenko <gaponenko.mm@gmail.com>
2021-01-17 13:33:59 +0200

    First Screen

--- components/main-page/first-screen.tsx
+++ components/main-page/first-screen.tsx
@@ -7,7 +7,15 @@ export default class FirstScreen extends Component {
     render() {
         return (
             <Section>
-                
+                <div className="container">
+                    <h1>
+                        Welcome<br />
+                        home, dear<br />
+                        traveler!
+                    </h1>
+                </div>
+
+
             </Section>
         )
     }
@@ -20,4 +28,23 @@ const Section = styled.div`
     background-image: url(${background});
     background-size: cover;
     background-position: center;
+    .container {
+        height: 100%;
+    }
+    h1 {
+        position: relative;
+        top: 50%;
+        left: 20px;
+        margin: 0;
+        font-style: italic; 
+        transform: translate(0, -50%);
+        display: inline-block;
+        color: #E8D5C0;
+        font-size: 80px;
+        line-height: 1.08;
+        text-shadow: 0px 2px 3px rgba(0,0,0,0.87);
+        @media (max-width: 1999px) {
+            font-size: 65px;
+        }
+    }
 `
--- pages/_document.js
+++ pages/_document.js
@@ -4,14 +4,12 @@ import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
     static getInitialProps({ renderPage }) {
+
+        // Styled-components setup START
         // Step 1: Create an instance of ServerStyleSheet
         const sheet = new ServerStyleSheet();
-
         // Step 2: Retrieve styles from components in the page
-        const page = renderPage((App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        );
-
+        const page = renderPage((App) => (props) => sheet.collectStyles(<App {...props} />));
         // Step 3: Extract the styles as <style> tags
         const styleTags = sheet.getStyleElement();
 
