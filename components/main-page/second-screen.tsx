e8003508acff20c8199d418ac58819b2abd39a14
Max Haponenko <gaponenko.mm@gmail.com>
2021-03-18 14:11:37 +0200

    Rooms section

--- components/main-page/second-screen.tsx
+++ components/main-page/second-screen.tsx
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
-import background from 'public/images/first-screen-bg.jpg';
-import BookingModule from 'components/booking/booking-module';
+import image from 'public/images/first-screen-bg.jpg'
+import image1 from 'public/images/image_1.jpg';
+import image2 from 'public/images/image_2.jpg';
+import image3 from 'public/images/image_3.jpg';
 
 export default class SecondScreen extends Component {
     render() {
         return (
             <Section>
-                <div className="container main-page__rooms">
-
+                <div className="container">
+                    <h3>ROOMS</h3>
+                    <div className="rooms-container">
+                        <div className="grid-item">
+                            <div className="room-1"></div>
+                        </div>
+                        <div className="grid-item">
+                            <div className="room-2"></div>
+                        </div>
+                        <div className="grid-item">
+                            <div className="room-3"></div>
+                        </div>
+                        <div className="grid-item">
+                            <div className="room-4"></div>
+                        </div>
+                    </div>
                 </div>
             </Section>
         )
@@ -23,23 +39,52 @@ const Section = styled.div`
     .container {
         height: 100%;
         position: relative;
+        padding-top: 100px;
+        padding-bottom: 140px;
     }
-    h1 {
-        position: relative;
-        top: 50%;
-        left: 20px;
-        margin: 0;
-        font-style: italic; 
-        transform: translate(0, -50%);
-        display: inline-block;
-        color: #E8D5C0;
-        font-size: 80px;
-        line-height: 1.08;
-        text-shadow: 0px 2px 3px rgba(0,0,0,0.87);
-        @media (max-width: 1999px) {
-            font-size: 65px;
+    h3 {
+        font-size: 35px;
+        text-align: center;
+        font-weight: normal;
+        margin-bottom: 70px;
+    }
+    .rooms-container {
+        display: flex;
+        justify-content: space-between;
+        margin-left: -10px;
+        margin-right: -10px;
+        
+        > .grid-item {
+            width: 25%;
+            padding: 10px;
+            height: 460px;
+            
+            > div {
+                position: relative;
+                height: 100%;
+                width: 100%;
+                
+                background-position: center;
+                background-size: cover;
+                background-repeat: no-repeat;
+                background-clip: content-box;
+                border-radius: 15px;
+                box-shadow: 0px 2px 2px rgba(0,0,0,0.5);
+                cursor: pointer;
+                transition: all 100ms ease-in-out;
+                
+                &:hover {
+                    transform: translate(0, -5px);
+                    
+                }
+            }
+            > .room-1 { background-image: url(${image}); }
+            > .room-2 { background-image: url(${image1}); }
+            > .room-3 { background-image: url(${image2}); }
+            > .room-4 { background-image: url(${image3}); }
         }
     }
+    
     .booking-module-container {
         width: 887px;
         position: absolute;
