f971f137f20ed68129d7ab717736f8e037c50edb
Max Haponenko <gaponenko.mm@gmail.com>
2021-03-18 21:53:39 +0200

    Rooms section in progress

--- components/main-page/second-screen.tsx
+++ components/main-page/second-screen.tsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
-import image from 'public/images/first-screen-bg.jpg'
-import image1 from 'public/images/image_1.jpg';
-import image2 from 'public/images/image_2.jpg';
-import image3 from 'public/images/image_3.jpg';
+import image from 'public/images/image_5.jpg'
+import image1 from 'public/images/image_7.jpg';
+import image2 from 'public/images/image_4.jpg';
+import image3 from 'public/images/image_6.jpg';
 
 export default class SecondScreen extends Component {
     render() {
@@ -14,16 +14,40 @@ export default class SecondScreen extends Component {
                     <h3>ROOMS</h3>
                     <div className="rooms-container">
                         <div className="grid-item">
-                            <div className="room-1"></div>
+                            <div className="room-item room-1">
+                                <div className="bottom-container">
+                                    <div className="room-name">Lord`s Chambers</div>
+                                    <div className="room-guests">2 guests</div>
+                                    <div className="room-price">from <b>500 UAH</b> for <b>2 guests</b></div>
+                                </div>
+                            </div>
                         </div>
                         <div className="grid-item">
-                            <div className="room-2"></div>
+                            <div className="room-item room-2">
+                                <div className="bottom-container">
+                                    <div className="room-name">Cell Monks</div>
+                                    <div className="room-guests">4 guests</div>
+                                    <div className="room-price orange">from <b>250 UAH</b> for <b>1 guest</b></div>
+                                </div>
+                            </div>
                         </div>
                         <div className="grid-item">
-                            <div className="room-3"></div>
+                            <div className="room-item room-3">
+                                <div className="bottom-container">
+                                    <div className="room-name">Knights of the Guild</div>
+                                    <div className="room-guests">10 guests</div>
+                                    <div className="room-price orange">from <b>200 UAH</b> for <b>1 guest</b></div>
+                                </div>
+                            </div>
                         </div>
                         <div className="grid-item">
-                            <div className="room-4"></div>
+                            <div className="room-item room-4">
+                                <div className="bottom-container">
+                                    <div className="room-name">Den of Thiefes</div>
+                                    <div className="room-guests">12 guests</div>
+                                    <div className="room-price orange">from <b>180 UAH</b> for <b>1 guest</b></div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -59,10 +83,13 @@ const Section = styled.div`
             padding: 10px;
             height: 460px;
             
-            > div {
+            > .room-item {
                 position: relative;
                 height: 100%;
                 width: 100%;
+                display: flex;
+                flex-direction: column;
+                overflow: hidden;
                 
                 background-position: center;
                 background-size: cover;
@@ -77,11 +104,56 @@ const Section = styled.div`
                     transform: translate(0, -5px);
                     
                 }
+
+                > .bottom-container {
+                    height: 50%;
+                    position: relative;
+                    margin-top: auto;
+                    padding: 20px;
+                    padding-top: 130px;
+                    .room-name {
+                        font-size: 22px;
+                    }
+                    .room-guests {
+                        font-size: 15px;
+                        margin-top: 3px;
+                    }
+                    .room-price {
+                        font-size: 17px;
+                        margin-top: 13px;
+                        b {
+                            font-weight: 600;
+                        }
+                        &.orange b {
+                            color: #FA9917;
+                        }
+                    }
+                }
             }
-            > .room-1 { background-image: url(${image}); }
-            > .room-2 { background-image: url(${image1}); }
-            > .room-3 { background-image: url(${image2}); }
-            > .room-4 { background-image: url(${image3}); }
+
+            /* > .room-item {
+                display: flex;
+                flex-direction: column;
+                overflow: hidden;
+                
+            } */
+
+            > .room-1 { 
+                background-image: url(${image}); 
+                > .bottom-container { background: linear-gradient(180deg, rgba(176,176,176,0) 0%, rgba(176,176,176,0.5) 50%, rgba(176,176,176,1) 100%); }
+            }
+            > .room-2 { 
+                background-image: url(${image1}); 
+                > .bottom-container { background: linear-gradient(180deg, rgba(176,176,176,0) 0%, rgba(176,176,176,0.5) 50%, rgba(176,176,176,1) 100%); }
+            }
+            > .room-3 { 
+                background-image: url(${image2}); 
+                > .bottom-container { background: linear-gradient(180deg, rgba(176,176,176,0) 0%, rgba(176,176,176,0.5) 50%, rgba(176,176,176,1) 100%); }
+            }
+            > .room-4 { 
+                background-image: url(${image3}); 
+                > .bottom-container { background: linear-gradient(180deg, rgba(176,176,176,0) 0%, rgba(176,176,176,0.5) 50%, rgba(176,176,176,1) 100%); }
+            }
         }
     }
     
