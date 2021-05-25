fe68d678a4490ac43272412eaddb4efc8f041bc1
Max Haponenko <gaponenko.mm@gmail.com>
2021-05-25 22:27:13 +0300

    Custom slider in progress

--- components/image-slider/image-slider.tsx
+++ components/image-slider/image-slider.tsx
@@ -28,7 +28,7 @@ const getAnimation = (items) => (index) => {
         to: {
             transform: isCenter ? `translate(${centerPosition}) scale(1)` : isLeft ? `translate(${leftPosition}) scale(0.25)` : `translate(${rightPosoition}) scale(0.25)`,
         },
-        delay: index * 50,
+        delay: index * 0,
 
         config: config.default,
     }
@@ -42,20 +42,38 @@ export default function ImageSlider() {
         2: false,
         3: false
     })
+    let [fullScreenMode, setFullScreenMode] = useState(false)
 
     const [springs, set] = useSprings(Object.values(items).length, getAnimation(items))
 
     useEffect(() => {
         set(getAnimation(items) as any)
+
     }, [items])
 
+    useEffect(() => {
+        // make closest images be at the top
+        const centerImageIndex = Object.values(items).indexOf(true)
+        const leftImageIndex = centerImageIndex - 1
+        const rightImageIndex = centerImageIndex + 1
+
+        document.querySelectorAll(`.animated-${leftImageIndex}`).forEach((item) => {
+            (item as any).style['z-index'] = 2
+        })
+        document.querySelectorAll(`.animated-${rightImageIndex}`).forEach((item) => {
+            (item as any).style['z-index'] = 2
+        })
+
+    }, [])
+
     const handleClick = (index, event) => {
+        event.preventDefault();
 
         const centerImageIndex = Object.values(items).indexOf(true)
 
         const isCenterItem = items[index] === true
         if (isCenterItem) {
-            return
+            setFullScreenMode(true)
         } else {
             let newPosition = {
                 0: false,
@@ -67,22 +85,41 @@ export default function ImageSlider() {
             setItems(newPosition)
 
             // Update indexes to lie one on top of another
-            debugger
-            if (centerImageIndex >= index) {
-                let nextImage = document.getElementById('animated-'+ (index + 1))
-                nextImage.style.zIndex = (parseInt(nextImage.style.zIndex) + 1).toString()
-            } else {
-                let prevImage = document.getElementById('animated-'+ (index - 1))
-                prevImage.style.zIndex = (parseInt(prevImage.style.zIndex) - 1).toString()
-            }
+            document.querySelectorAll(`.animated-${centerImageIndex}`).forEach((item) => {
+                (item as any).style['z-index'] = maxZindex() + 1
+            })
         }
     }
 
-    return (
-        <Section>
+    const centerIndex = Object.values(items).indexOf(true)
+
+    const renderAnimatedImages = () => (
+        <>
             {springs.map((item, index) => (
-                <animated.div key={index} id={'animated-' + index} className="image" style={{ ...item, backgroundImage: `url(${images[index]})`, zIndex: 1 }} onClick={(event) => handleClick(index, event) }></animated.div>
+                <animated.div
+                    key={index}
+                    className={`image animated ${'animated-' + index} ${centerIndex === index ? 'center-image' : ''}`}
+                    style={{ ...item, backgroundImage: `url(${images[index]})`, zIndex: 1 }}
+                    onClick={(event) => {
+                        event.stopPropagation()
+                        handleClick(index, event)
+                    }}
+                >
+                </animated.div>
             ))}
+        </>
+    )
+
+    return (
+        <Section fullScreen={fullScreenMode}>
+            {renderAnimatedImages()}
+            <div className="full-screen-mode" onClick={(event) => {
+                setFullScreenMode(false)
+            }}>
+                <div className="wrapper">
+                    {renderAnimatedImages()}
+                </div>
+            </div>
         </Section>
     )
 }
@@ -103,9 +140,56 @@ const Section = styled.div`
         border-radius: 15px;
         box-shadow: 0 2px 4px rgba(0,0,0,0.5);
         /* border: 1px solid rgba(255,255,255,0.2); */
-
+        filter: grayscale(1);
         background-size: cover;
         background-position: center;
         z-index: 1;
+        cursor: pointer;
+        transition: filter 350ms ease-in-out;
+        &:hover {
+            filter: grayscale(0);
+        }
+        &.center-image {
+            filter: grayscale(0);
+        }
+    }
+    .full-screen-mode {
+        position: fixed;
+        width: 100vw;
+        height: 100vh;
+        background-color: rgba(0,0,0,0.9);
+        top: 0;
+        left: 0;
+        z-index: 100;
+        display: ${props => props.fullScreen ? 'block' : 'none'};
+        .wrapper {
+            position: relative;
+            width: 100%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            .image {
+                height: 800px;
+                width: 500px;
+                max-height: 80%;
+                max-width: 50%;
+            }
+        }
     }
 `
+
+function maxZindex() {
+    var elems = document.querySelectorAll('.image.animated');
+    var highest = Number.MIN_SAFE_INTEGER || -(Math.pow(2, 53) - 1);
+    for (var i = 0; i < elems.length; i++) {
+        var zindex = Number.parseInt(
+            document.defaultView.getComputedStyle(elems[i], null).getPropertyValue("z-index"),
+            10
+        );
+        if (zindex > highest) {
+            highest = zindex;
+        }
+    }
+    return highest;
+}
